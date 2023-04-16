855232184b07c9de8ab52a183f0f209916dca601
Alexey Troitsky <astroit3@mts.ru>
2023-04-16 21:12:29 +0300

    feat: add state to dish counter
    homework-3

--- src/components/Dish/Dish.jsx
+++ src/components/Dish/Dish.jsx
@@ -1,25 +1,36 @@
 import { Button } from "@/components/Button/Button";
-import React from "react";
+import React, {useState} from "react";
 
 import ThumbDown from "./images/thumb-down.svg";
 
 import styles from "./styles.module.scss";
 
 export const Dish = ({ dish }) => {
+  const [activeDishCount, setActiveDishCount] = useState(0);
+
   if (!dish) {
     return null;
   }
 
   const { name, ingredients } = dish;
 
+  const increaseCount = () => {
+    setActiveDishCount(activeDishCount >= 5 ? 5 : activeDishCount + 1)
+  }
+
+  const decreaseCount = () => {
+    setActiveDishCount(activeDishCount <= 0 ? 0 : activeDishCount - 1)
+  }
+
+
   return (
     <div>
       <span>{name}</span>
-      <Button className={styles.decrementAction} type="secondary">
+      <Button className={styles.decrementAction} onClick={decreaseCount} type="secondary">
         -
       </Button>
-      0
-      <Button className={styles.incrementAction} type="primary">
+      {activeDishCount}
+      <Button className={styles.incrementAction} onClick={increaseCount} type="primary">
         +
       </Button>
     </div>
